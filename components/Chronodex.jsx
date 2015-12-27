eebc9e7b7dfc56810c5326c79ee900de0ad107bb
naush <li-hsuan@namely.com>
2015-12-27 13:44:32 -0500

    Simple events

--- components/Chronodex.jsx
+++ components/Chronodex.jsx
@@ -4,13 +4,30 @@ import Style from './Style.jsx'
 import Slice from 'simple-slice'
 
 class Chronodex extends React.Component {
+  radius(eventCount) {
+    let { radius } = this.props
+    return radius * (1 + eventCount * 0.2)
+  }
+
+  hourToDegree (hour) {
+    let degree = 0
+    if (hour < 12) {
+      degree = hour * 30
+    }
+    return degree
+  }
 
   render () {
-    let { size, center, radius } = this.props
+    let { size, center, radius, events } = this.props
 
     let style = new Style()
     let viewBox = [0, 0, size, size].join(' ')
-    let props = { center: center, color: 'white', strokeWidth: 0.5, opacity: 1.0 }
+    let props = {
+      center: center,
+      color: 'white',
+      strokeWidth: 0.5,
+      opacity: 1.0
+    }
 
     return (
       <div style={style.container()}>
@@ -24,16 +41,36 @@ class Chronodex extends React.Component {
                 <Index center={center} />
                 <Slice {...props} radius={radius} start={0} end={30} />
                 <Slice {...props} radius={radius * 1.2} start={30} end={60} />
-                <Slice {...props} radius={radius * 1.6} start={60} end={90} />
+                <Slice {...props} radius={radius * 1.4} start={60} end={90} />
                 <Slice {...props} radius={radius} start={90} end={120} />
                 <Slice {...props} radius={radius * 1.2} start={120} end={150} />
-                <Slice {...props} radius={radius * 1.6} start={150} end={180} />
+                <Slice {...props} radius={radius * 1.4} start={150} end={180} />
                 <Slice {...props} radius={radius} start={180} end={210} />
                 <Slice {...props} radius={radius * 1.2} start={210} end={240} />
-                <Slice {...props} radius={radius * 1.6} start={240} end={270} />
+                <Slice {...props} radius={radius * 1.4} start={240} end={270} />
                 <Slice {...props} radius={radius} start={270} end={300} />
                 <Slice {...props} radius={radius * 1.2} start={300} end={330} />
-                <Slice {...props} radius={radius * 1.6} start={330} end={360} />
+                <Slice {...props} radius={radius * 1.4} start={330} end={360} />
+                {
+                  events.map((event, index) => {
+                    let [start, end, descriptions] = event
+                    let eventCount = descriptions.split(",").length - 1
+
+                    return (
+                      <Slice
+                        key={index}
+                        center={center}
+                        radius={this.radius(eventCount)}
+                        color='deepskyblue'
+                        opacity={0.5}
+                        strokeWidth={2.0}
+                        strokeColor='deepskyblue'
+                        start={this.hourToDegree(start)}
+                        end={this.hourToDegree(end)}
+                      />
+                    )
+                  })
+                }
                 <circle
                   cx={center}
                   cy={center}
--- entry.js
+++ entry.js
@@ -1,9 +1,20 @@
 import React from 'react'
 import Chronodex from './components/Chronodex.jsx'
 
+let events = [
+  [12, 1, 'Lunch'],
+  [1, 3, 'Marketing Meeting'],
+  [3, 4, 'Coffee'],
+  [4, 5, 'Email'],
+  [5, 6, 'Commute,Reading,Music'],
+  [7, 8, 'Dinner,Netflix'],
+  [8, 12, 'Sleep']
+]
+
 React.render(<Chronodex
                 title='Chronodex'
                 size={128}
                 center={64}
                 radius={32}
+                events={events}
               />, document.querySelector('#chronodex'))
